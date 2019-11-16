7c3bf33e35b57a4c2001fb025b67fc86aa16ed3c
Dies-govorov <denis.2007.10.28@gmail.com>
2019-11-16 10:21:19 +0200

    First commit

---
+++ Assets/scripts/Bomb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+	private Animator anim;
+	private const string isBlowValName = "isBlowing";
+	private const int blowTimer = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+		Destroy (gameObject, 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ifCollision(){
+		anim.SetBool (isBlowValName, true);
+		Destroy (gameObject, blowTimer);
+	}
+}
---
+++ Assets/scripts/camfollover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class camfollover : MonoBehaviour {
+
+	public Transform player;
+
+	
+	// Update is called once per frame
+	public void FixedUpdate () {
+		transform.position = new Vector3 (player.position.x, player.position.y,transform.position.z);
+	}
+}
